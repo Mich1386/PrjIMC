8e58e108b2723dae33c4b9cdae359bef50f3234e
Mich1386 <desouzagamamichele475@gmail.com>
2019-11-11 14:00:40 -0300

    inicio do projeto

---
+++ src/app/tab1/tab1.page.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tab1',
+  templateUrl: 'tab1.page.html',
+  styleUrls: ['tab1.page.scss']
+})
+export class Tab1Page {
+
+  private altura: number;
+  private resultado: number;
+
+  constructor() {}
+
+  calcularM(){
+    this.resultado = (this.altura -100) * 0.9;
+  }
+
+  exibeResultado(): number{
+    return this.resultado;
+  }
+
+}
